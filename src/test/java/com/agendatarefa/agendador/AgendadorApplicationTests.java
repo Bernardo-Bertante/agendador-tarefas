22d647d97783c3a06c00990027ec50606c17b158
Bernardo Bertante <bernardo.sctp@gmail.com>
2025-12-11 17:35:37 -0300

    esqueleto inicial do projeto

---
+++ src/test/java/com/agendatarefa/agendador/AgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendatarefa.agendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
