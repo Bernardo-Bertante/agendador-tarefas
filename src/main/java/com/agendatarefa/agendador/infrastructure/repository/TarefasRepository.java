6696371f1cb8c239199a7938d99f236724c2f14c
Bernardo Bertante <bernardo.sctp@gmail.com>
2026-01-05 18:00:05 -0300

    alterado a busca por periodo, agora valida por status tambem

--- src/main/java/com/agendatarefa/agendador/infrastructure/repository/TarefasRepository.java
+++ src/main/java/com/agendatarefa/agendador/infrastructure/repository/TarefasRepository.java
@@ -1,6 +1,7 @@
 package com.agendatarefa.agendador.infrastructure.repository;
 
 import com.agendatarefa.agendador.infrastructure.entity.TarefasEntity;
+import com.agendatarefa.agendador.infrastructure.enums.StatusNotificacaoEnum;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,7 @@ import java.util.List;
 @Repository
 public interface TarefasRepository extends MongoRepository<TarefasEntity, String> {
 
-    List<TarefasEntity> findByDataEventoBetween(LocalDateTime dataInicio, LocalDateTime dataFim);
+    List<TarefasEntity> findByDataEventoBetweenAndStatusNotificacaoEnum(LocalDateTime dataInicio, LocalDateTime dataFim, StatusNotificacaoEnum status);
 
     List<TarefasEntity> findByEmailUsuario(String email);
 
